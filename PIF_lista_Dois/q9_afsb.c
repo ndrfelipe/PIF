2ba3f625b67440f9c52bac5a3ce75884db9cdecd
ndrfelipe <ndrfelipebraga@gmail.com>
2025-03-08 18:25:06 -0300

    q9

---
+++ PIF_lista_Dois/q9_afsb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int password = 2002, pass;
+
+    do{
+        scanf("%d", &pass);
+        if (pass != password){
+            printf("Senha Invalida\n");
+        }
+        
+    } while (pass != password);
+    
+    printf("Acesso Permitido\n");
+    
+
+     
+    return 0;
+}
