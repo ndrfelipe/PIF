f9d355f260b015f2047de97acf3a7d5363dd2b29
ndrfelipe <ndrfelipebraga@gmail.com>
2025-03-13 11:46:42 -0300

    fibonacci

--- PIF_lista_Dois/q3_afsb.c
+++ PIF_lista_Dois/q3_afsb.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
 int main(){
-    int a = 0, b = 1, varTemp, value;
 
-    scanf("%d", &value);
+    int numeroQueSeraImprimido = 0, numeroParaCalculo = 1, variavelAuxiliarParaCalculo, valorDeEntrada_N;
+    scanf("%d", &valorDeEntrada_N);
 
-    for (int i = 0; i < value; i++){
-        printf("%d", a);
-        if (i < value-1){
-            printf(" ");
-        }
-        
-        varTemp = a + b;
-        a = b;
-        b = varTemp;
-    }
+    if (valorDeEntrada_N > 0 && valorDeEntrada_N < 46){
+        for(int i; i < valorDeEntrada_N; i++){
+            printf("%d", numeroQueSeraImprimido);
+            if( i < valorDeEntrada_N - 1){
+                printf(" ");
+            }
     
+            variavelAuxiliarParaCalculo = numeroQueSeraImprimido + numeroParaCalculo;
+            numeroQueSeraImprimido = numeroParaCalculo;
+            numeroParaCalculo = variavelAuxiliarParaCalculo;
+        }
+    }
+    printf("\n");
     return 0;
 }
